85bb96f739b2d4707b40a20bc10b3d41f6e6ef25
brugrieco <brunonicolasgrieco@gmail.com>
2022-10-18 12:38:58 -0300

    acomodando app.js 4

--- src/app.js
+++ src/app.js
@@ -1,31 +1,31 @@
 // const express = require('express')
-// const path = require('path')
+const path = require('path')
 // const app = express()
-// const methodOverride = require('method-override')
-// const session = require('express-session')
-// const cookies = require('cookie-parser')
-// const userLoggedMiddleware = require('./middleware/userLoggedMiddleware')
-// const productsRouter = require('./routes/productsRouter.js');
-// const usersRouter = require('./routes/usersRouter.js');
-// const mainRouter = require('./routes/mainRouter.js');
+const methodOverride = require('method-override')
+const session = require('express-session')
+const cookies = require('cookie-parser')
+const userLoggedMiddleware = require('./middleware/userLoggedMiddleware')
+const productsRouter = require('./routes/productsRouter.js');
+const usersRouter = require('./routes/usersRouter.js');
+const mainRouter = require('./routes/mainRouter.js');
 
-// const port = 3000;
+const port = 3000;
 
-// app.use(express.static(path.resolve(__dirname, '../public')))
-// app.set('view engine', 'ejs');
-// app.set('views', path.resolve(__dirname, 'views'))
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-// app.use(methodOverride("_method"));
+app.use(express.static(path.resolve(__dirname, '../public')))
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'views'))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride("_method"));
 
-// app.use(session({
-//     secret: 'secret',	
-//     resave: false,
-//     saveUninitialized: false    
-// }))
+app.use(session({
+    secret: 'secret',	
+    resave: false,
+    saveUninitialized: false    
+}))
 
-// app.use(cookies())
-// app.use(userLoggedMiddleware)
+app.use(cookies())
+app.use(userLoggedMiddleware)
 
 // app.use('/', mainRouter);
 // app.use('/products', productsRouter);

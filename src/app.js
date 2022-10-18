2b20391776f2a2d68426c34af8ee704ceca62707
brugrieco <brunonicolasgrieco@gmail.com>
2022-10-18 12:36:38 -0300

    acomodando app.js 3

--- src/app.js
+++ src/app.js
@@ -40,9 +40,7 @@ app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000, () => {console.log('listening on port 3000');});
 
-app.get('/', (req, res) => {
-    res.send('mi server funcionando');
-})
+app.use('/', mainRouter);
 
 
 

611fd7b99e83fe9f071bb83e2ec1341ce5fc094a
brugrieco <brunonicolasgrieco@gmail.com>
2022-10-18 11:27:56 -0300

    modificando models/index.js

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -8,9 +8,16 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.JAWSDB_MARIA_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_MARIA_URL);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }

087fd6a7899c4866423f44ce07cc5ee3c6136d72
brugrieco <brunonicolasgrieco@gmail.com>
2022-10-18 12:11:25 -0300

    acomodando app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
 const express = require('express')
+const path = require('path')
+const app = express()
+const methodOverride = require('method-override')
 const session = require('express-session')
 const cookies = require('cookie-parser')
-const path = require('path')
 const userLoggedMiddleware = require('./middleware/userLoggedMiddleware')
-
-
-const app = express()
-
-
-const port = process.env.PORT || 3000;
-const methodOverride = require('method-override')
-
 const productsRouter = require('./routes/productsRouter.js');
 const usersRouter = require('./routes/usersRouter.js');
 const mainRouter = require('./routes/mainRouter.js');
+
+const port = 3000;
+
+app.use(express.static(path.resolve(__dirname, '../public')))
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'views'))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride("_method"));
 
 app.use(session({
     secret: 'secret',	
@@ -22,26 +25,22 @@ app.use(session({
 }))
 
 app.use(cookies())
-
 app.use(userLoggedMiddleware)
 
-app.use(express.static(path.resolve(__dirname, '../public')))
-
-app.set('view engine', 'ejs');
-
-app.set('views', path.join(__dirname, 'views'))
-
-app.use(methodOverride("_method"));
-
-app.use(express.urlencoded({ extended: false }));
-
-app.use(express.json());
-
 app.use('/', mainRouter);
-
 app.use('/products', productsRouter);
-
 app.use('/users', usersRouter);
 
+app.listen(process.env.PORT || port, () => console.log(`servidor funcionando en el puerto ${port}! `))
 
-app.listen(port, () => console.log(`servidor funcionando en el puerto ${port}! `))
+
+
+
+
+
+
+
+
+
+
+
